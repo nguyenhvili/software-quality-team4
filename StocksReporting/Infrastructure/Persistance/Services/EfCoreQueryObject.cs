0379cc92bfa034d38029fcdfda93f881f0f9bbe9
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-12 14:22:40 +0200

    feat: implement application services classes

---
+++ StocksReporting/Infrastructure/Persistance/Services/EfCoreQueryObject.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistR.Attributes.Base;
+using StocksReporting.Application.Services;
+
+namespace StocksReporting.Infrastructure.Persistance.Services;
+
+[Register(ServiceLifetime.Transient, typeof(IQueryObject<>))]
+public class EfCoreQueryObject<TAggregate> : QueryObject<TAggregate> where TAggregate : class
+{
+    private readonly DepartureCustomerServicesDbContext _dbContext;
+
+    public EfCoreQueryObject(DepartureCustomerServicesDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        _query = _dbContext.Set<TAggregate>().AsQueryable();
+    }
+
+    public override async Task<IEnumerable<TAggregate>> ExecuteAsync()
+    {
+        return await _query.ToListAsync();
+    }
+}

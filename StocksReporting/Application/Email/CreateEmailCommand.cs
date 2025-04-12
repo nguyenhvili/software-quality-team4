98bf12de415d431996f2a2e3f3c281f27782585b
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-12 20:44:37 +0200

    feat: implement create email command

---
+++ StocksReporting/Application/Email/CreateEmailCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace StocksReporting.Application.Email;
+
+public record CreateEmailCommand(Guid Id, string EmailValue)
+{
+    public record CreatedEmail(
+        Guid Id,
+        string EmailValue
+    );
+
+    public record Result(CreatedEmail Email);
+}
---
+++ StocksReporting/Application/Email/CreateEmailCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using ErrorOr;
+using StocksReporting.Application.Services;
+
+namespace StocksReporting.Application.Email;
+
+public class CreateEmailCommandHandler
+{
+    private readonly IRepository<Domain.Email.Email> _repository;
+
+    public CreateEmailCommandHandler(IRepository<Domain.Email.Email> repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<ErrorOr<CreateEmailCommand.Result>> Handle(CreateEmailCommand command)
+    {
+        var emailResult = Domain.Email.Email.Create(
+            command.Id,
+            command.EmailValue
+        );
+
+        if (emailResult.IsError)
+        {
+            return emailResult.Errors;
+        }
+        var email = emailResult.Value;
+
+        await _repository.InsertAsync(email);
+        await _repository.CommitAsync();
+
+        return new CreateEmailCommand.Result(
+            new CreateEmailCommand.CreatedEmail(
+                email.Id.Value,
+                email.EmailValue.Value
+                )
+            );
+    }
+}

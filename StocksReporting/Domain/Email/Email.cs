27070ed497e1345d5b2a381d22d3d2b1a41d8e4e
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-12 14:04:25 +0200

    feat: implement email domain classes

---
+++ StocksReporting/Domain/Email/Email.cs
@@ -0,0 +1,24 @@
+﻿using StocksReporting.Domain.Common;
+using StocksReporting.Domain.Email.ValueObjects;
+
+namespace StocksReporting.Domain.Email;
+
+public class Email : AggregateRoot<EmailId>
+{
+    public EmailValue EmailValue { get; private set; }
+
+    public Email()
+    {
+
+    }
+
+    private Email(EmailId emailId, EmailValue emailValue) : base(emailId)
+    {
+        EmailValue = emailValue;
+    }
+
+    public static Email Create(Guid id, string email)
+    {
+        return new Email(EmailId.Create(id), EmailValue.Create(email));
+    }
+}
---
+++ StocksReporting/Domain/Email/ValueObjects/EmailValue.cs
@@ -0,0 +1,20 @@
+﻿using StocksReporting.Domain.Common;
+
+namespace StocksReporting.Domain.Email.ValueObjects;
+
+public class EmailValue : ValueObject
+{
+    public string Value { get; private set; }
+
+    private EmailValue(string value)
+    {
+        Value = value;
+    }
+
+    public static EmailValue Create(string value) => new(value);
+
+    protected override IEnumerable<object?> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}

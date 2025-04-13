2b44fb306fd25133b1a41b73a06985ef88ce8470
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-13 17:50:35 +0200

    feat: add list emails command + endpoint

---
+++ StocksReporting/Application/Email/ListEmailsCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using ErrorOr;
+using StocksReporting.Application.Services;
+
+namespace StocksReporting.Application.Email;
+
+public class ListEmailsCommandHandler
+{
+    private readonly IQueryObject<Domain.Email.Email> _queryObject;
+
+    public ListEmailsCommandHandler(IQueryObject<Domain.Email.Email> queryObject)
+    {
+        _queryObject = queryObject;
+    }
+
+    public async Task<ErrorOr<ListEmailsCommand.Result>> Handle(ListEmailsCommand command)
+    {
+        var emails = await _queryObject.Page(command.Page, command.PageSize).ExecuteAsync();
+
+        var result = emails.Select(email => new ListEmailsCommand.Email(email.Id.Value, email.EmailValue.Value));
+
+        return new ListEmailsCommand.Result(result);
+    }
+}
---
+++ StocksReporting/Presentation/Email/ListEmailsEndpoint.cs
@@ -0,0 +1,33 @@
+﻿using ErrorOr;
+using StocksReporting.Application.Email;
+using Wolverine;
+using Wolverine.Http;
+
+namespace StocksReporting.Presentation.Email;
+
+public class ListEmailsEndpoint
+{
+    [WolverineGet("emails")]
+    public static async Task<IResult> ListEmailsAsync(Request request, IMessageBus sender)
+    {
+        var command = new ListEmailsCommand(request.Page, request.PageSize);
+
+        var result = await sender.InvokeAsync<ErrorOr<ListEmailsCommand.Result>>(command);
+
+        return result.Match(
+            value => Results.Ok(
+                new Response(
+                    value.Emails.Select(email => new Response.Email(email.Id, email.EmailValue))
+                )
+            ),
+            errors => Results.BadRequest(errors.Select(e => e.Code))
+        );
+    }
+
+    public record Request(int Page, int PageSize);
+
+    public record Response(IEnumerable<Response.Email> Emails)
+    {
+        public record Email(Guid Id, string EmailValue);
+    }
+}

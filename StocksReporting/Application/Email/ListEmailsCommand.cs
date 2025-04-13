9cb41b23d201d7344507e966e421e8f2d62bbe93
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-13 17:50:35 +0200

    feat: add list emails command + endpoint

---
+++ StocksReporting/Application/Email/ListEmailsCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace StocksReporting.Application.Email;
+
+public record ListEmailsCommand(int Page, int PageSize)
+{
+    public record Result(IEnumerable<Email> Emails);
+
+    public record Email(Guid Id, string EmailValue);
+}

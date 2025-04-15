563c369749be9317b028cd793491a47083029c74
Anna Vítová <514266@muni.cz>
2025-04-15 18:15:12 +0200

    feat: add migrations

---
+++ StocksReporting/Infrastructure/Persistence/Migrations/20250415161441_AddReport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StocksReporting.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    ReportId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FilePath = table.Column<string>(type: "TEXT", maxLength: 500, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.ReportId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}

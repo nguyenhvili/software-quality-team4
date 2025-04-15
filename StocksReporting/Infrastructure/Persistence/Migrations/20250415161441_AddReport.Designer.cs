a0805e798613003189cb01da45ffb27887abe45c
Anna Vítová <514266@muni.cz>
2025-04-15 18:15:12 +0200

    feat: add migrations

--- StocksReporting/Infrastructure/Persistence/Migrations/20250415161441_AddReport.Designer.cs
+++ StocksReporting/Infrastructure/Persistence/Migrations/20250415161441_AddReport.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StocksReporting.Infrastructure;
 
@@ -10,9 +11,11 @@ using StocksReporting.Infrastructure;
 namespace StocksReporting.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(StocksReportingDbContext))]
-    partial class StocksReportingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250415161441_AddReport")]
+    partial class AddReport
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.8");

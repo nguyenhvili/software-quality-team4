a8ea90ff841f2fce534d1ea56e9c35fd7f9abe72
Martin Stalmach <martin.stalmach21@gmail.com>
2025-04-16 14:28:43 +0200

    feat: implement csv writer

---
+++ StocksReporting/Application/Services/CsvService/CsvWriter.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace StocksReporting.Application.Services.CsvService;
+
+public class CsvWriter
+{
+    public void Write(Domain.Report.Report report)
+    {
+        if (report is null)
+        {
+            return;
+        }
+        var path = report.ReportPathValue.PathValue;
+        var holdings = report.Holdings;
+
+        using var writer = new StreamWriter(path, false, Encoding.UTF8);
+        using var csv = new CsvHelper.CsvWriter(writer, CultureInfo.InvariantCulture);
+
+        csv.WriteField("company");
+        csv.WriteField("ticker");
+        csv.WriteField("shares (%)");
+        csv.WriteField("weight (%)");
+        csv.NextRecord();
+
+        foreach (var holding in holdings)
+        {
+            var company = holding.Company.Name;
+            var ticker = holding.Ticker.Value;
+            var shares = holding.Shares.Value;
+            var sharesPercent = holding.SharesPercent?.Value ?? 0;
+            var weight = holding.Weight.Value;
+
+            string formattedShares;
+            if (sharesPercent > 0)
+            {
+                formattedShares = $"{shares} (🔺{sharesPercent}%)";
+            }
+            else if (sharesPercent < 0)
+            {
+                formattedShares = $"{shares} (🔻{Math.Abs(sharesPercent)}%)";
+            }
+            else
+            {
+                formattedShares = shares.ToString();
+            }
+
+            csv.WriteField(company);
+            csv.WriteField(ticker);
+            csv.WriteField(formattedShares);
+            csv.WriteField($"{weight} %");
+            csv.NextRecord();
+        }
+    }
+}

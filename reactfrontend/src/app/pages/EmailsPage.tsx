32d1aff81ff21fb370701b721bd89f3400d995c4
Lucia Marušinová <xmarusin@fi.muni.cz>
2025-04-21 19:57:13 +0200

    feat: pages init

---
+++ reactfrontend/src/app/pages/EmailsPage.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react";
+import AppLayout from "../layouts/AppLayout";
+
+type EmailsPageProps = {};
+
+const EmailsPage: FC<EmailsPageProps> = (props) => {
+  const {} = props;
+
+  return (
+    <AppLayout>
+      <div>emails page</div>
+    </AppLayout>
+  );
+};
+
+export default EmailsPage;

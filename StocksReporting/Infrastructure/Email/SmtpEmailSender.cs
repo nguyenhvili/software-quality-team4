19d6d8686eec913a1ead82850a99de63d605c56f
Martin Stalmach <martin.stalmach21@gmail.com>
2025-05-06 10:13:09 +0200

    fix: move email variables to appsettings

--- StocksReporting/Infrastructure/Email/SmtpEmailSender.cs
+++ StocksReporting/Infrastructure/Email/SmtpEmailSender.cs
@@ -7,10 +7,17 @@ namespace StocksReporting.Infrastructure.Email;
 
 public class SmtpEmailSender : IEmailSender
 {
+    private readonly IConfiguration _configuration;
+
+    public SmtpEmailSender(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public async Task SendEmailAsync(string to, string subject, string body, string[] attachments)
     {
         var message = new MimeMessage();
-        message.From.Add(MailboxAddress.Parse("no-reply@stocksreporting.com"));
+        message.From.Add(MailboxAddress.Parse(_configuration["EmailSettings:SenderEmail"]));
         message.To.Add(MailboxAddress.Parse(to));
         message.Subject = subject;
 
@@ -24,8 +31,8 @@ public class SmtpEmailSender : IEmailSender
         message.Body = builder.ToMessageBody();
 
         using var smtp = new SmtpClient();
-        await smtp.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
-        await smtp.AuthenticateAsync("pv260reports@gmail.com", "gffd oyas dbhc hkhr");
+        await smtp.ConnectAsync(_configuration["EmailSettings:SmtpServer"], int.Parse(_configuration["EmailSettings:Port"] ?? "587"), SecureSocketOptions.StartTls);
+        await smtp.AuthenticateAsync(_configuration["EmailSettings:Username"], _configuration["EmailSettings:Password"]);
         await smtp.SendAsync(message);
         await smtp.DisconnectAsync(true);
     }

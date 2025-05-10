4fa1f9aefd9ee618fe1493694eba6d1bb82536d7
Anna Vítová <514266@muni.cz>
2025-05-10 11:23:29 +0200

    fix: add constructors

--- StocksReportingLibrary/Application/Report/ListReportsCommandHandler.cs
+++ StocksReportingLibrary/Application/Report/ListReportsCommandHandler.cs
@@ -4,11 +4,19 @@ using Microsoft.Extensions.Logging;
 
 namespace StocksReportingLibrary.Application.Report;
 
-public class ListReportsCommandHandler(IQueryObject<Domain.Report.Report> queryObject)
+public class ListReportsCommandHandler
 {
     private readonly IQueryObject<Domain.Report.Report> _queryObject;
     private readonly ILogger<ListReportsCommandHandler> _logger;
-    
+
+    public ListReportsCommandHandler(
+        IQueryObject<Domain.Report.Report> queryObject,
+        ILogger<ListReportsCommandHandler> logger)
+    {
+        _queryObject = queryObject;
+        _logger = logger;
+    }
+
     public async Task<ErrorOr<ListReportsCommand.Result>> Handle(ListReportsCommand command)
     {
         _logger.LogInformation("ListReportsCommandHandler with Page: {Page}, PageSize: {PageSize}",

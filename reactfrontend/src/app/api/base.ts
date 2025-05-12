bd2defa20ca92bc6d41d68b2381b6856655e5d65
Lucia Marušinová <marusinova.lucia@gmail.com>
2025-05-12 14:46:11 +0200

    feat: add BE url

--- reactfrontend/src/app/api/base.ts
+++ reactfrontend/src/app/api/base.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const base = axios.create({
-  baseURL: `http://localhost:5025/`, //add BE url
+  baseURL:
+    "https://pv260-ewdzffc2c5b0cqce.germanywestcentral-01.azurewebsites.net",
 });

4ad5c47ad3c32e928961fcbc037e4bb012bb6f3d
Tereza Helanová <xhelanov@fi.muni.cz>
2025-05-12 18:52:50 +0200

    feat: base name defined

--- reactfrontend/vite.config.js
+++ reactfrontend/vite.config.js
@@ -6,11 +6,12 @@ import svgrPlugin from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin(), tailwindcss(), svgrPlugin()],
-    build: {
-        manifest: true,
-      },
-      server: {
-        port: 3000,
-      },
+  base: '/software-quality-team4/',
+  plugins: [plugin(), tailwindcss(), svgrPlugin()],
+  build: {
+    manifest: true,
+  },
+  server: {
+    port: 3000,
+  },
 })

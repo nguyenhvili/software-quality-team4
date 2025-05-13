bbe5b5ac854b6fdfc0bf39c167d21f8dcef61ebb
Lucia Marušinová <marusinova.lucia@gmail.com>
2025-05-13 09:12:25 +0200

    feat: add vercel to config

--- reactfrontend/vite.config.js
+++ reactfrontend/vite.config.js
@@ -2,15 +2,16 @@ import { defineConfig } from 'vite';
 import plugin from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgrPlugin from 'vite-plugin-svgr';
+import vercel from 'vite-plugin-vercel';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin(), tailwindcss(), svgrPlugin()],
+    plugins: [plugin(), tailwindcss(), svgrPlugin(), vercel()],
     build: {
         manifest: true,
       },
       server: {
-        port: 3000,
+        port: process.env.PORT,
       },
 })

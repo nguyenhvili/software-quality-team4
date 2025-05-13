a4343880daf83d4830ab6f860fb6b20834b47bdd
Lucia Marušinová <marusinova.lucia@gmail.com>
2025-05-13 16:48:11 +0200

    fix: deleted commented code

--- StocksReporting/Program.cs
+++ StocksReporting/Program.cs
@@ -18,10 +18,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
-    //options.AddPolicy("AllowLocalhost3000",
-    //    policy => policy.WithOrigins("http://localhost:3000")
-    //        .AllowAnyHeader()
-    //        .AllowAnyMethod());
     options.AddPolicy("AllowVercel",
         policy => policy.WithOrigins("https://software-quality-team4-wine.vercel.app")
             .AllowAnyHeader()
@@ -107,7 +103,6 @@ builder.Services.AddScoped<StocksReportingLibrary.Application.Services.File.IPar
 
 var app = builder.Build();
 
-// app.UseCors("AllowLocalhost3000");
 app.UseCors("AllowVercel"); 
 
 using (var scope = app.Services.CreateScope())
